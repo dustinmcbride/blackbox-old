1633e571029c189317301492d9c62765d61d1d1e
Dustin McBride <dustinbm@gmail.com>
2015-11-04 22:20:52 -0800

    add thread to initializers

---
+++ vendor/python/writetolcd.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+# wrtitetolcd.py "message for line 1" "message for line 2"
+
+from Adafruit_CharLCD import Adafruit_CharLCD
+
+import sys
+
+total = len(sys.argv)
+cmdargs = str(sys.argv)
+
+lcd = Adafruit_CharLCD()
+
+lcd.begin(16, 1)
+lcd.clear()
+lcd.message(sys.argv[1])
+lcd.message("\n")
+lcd.message(sys.argv[2])
