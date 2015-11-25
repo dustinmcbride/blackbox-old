a2f7b58e7af1a117ca34c9a50ae8da7655ab1c98
Dustin McBride <dustinbm@gmail.com>
2015-11-24 22:16:01 -0800

    added loop run to lcdmessage when not planes

--- config/initializers/thread.rb
+++ config/initializers/thread.rb
@@ -18,7 +18,7 @@ while true == true
 
         # write airplane message to lcd
         if a == nil
-          system "sudo python ./vendor/python/writetolcd.py \"No planes in sight\" \" NA \" "
+          system "sudo python ./vendor/python/writetolcd.py \"No planes\" \"Loop Run: #{running} \" "
         elsif
           msg = a.getlcdmessage
           system "sudo python ./vendor/python/writetolcd.py \"#{msg[0]}\" \"R:#{msg[1]}\""#        end
